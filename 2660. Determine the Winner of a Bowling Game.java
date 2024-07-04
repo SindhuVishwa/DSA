79f05d5e6d558fefd74bf7880d2408dc079ef510
SindhuVishwa <48493113+SindhuVishwa@users.noreply.github.com>
2024-07-05 00:43:47 +0530

    Create 2660. Determine the Winner of a Bowling Game.java

---
+++ 2660. Determine the Winner of a Bowling Game.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/determine-the-winner-of-a-bowling-game/
+class Solution {
+    public int isWinner(int[] player1, int[] player2) {
+
+        int sum1 = player1[0];
+        int sum2 = player2[0];
+        for(int i = 1;i<player1.length;i++)
+        {
+            if(player1[i-1]==10 || (i>1 && player1[i-2]==10))
+            {
+                    sum1+=player1[i]*2;
+            }
+            else
+            {
+                sum1+=player1[i];
+            }
+            if(player2[i-1]==10 || (i>1 && player2[i-2]==10))
+            {
+                    sum2+=player2[i]*2;
+            }
+            else
+            {
+                sum2+=player2[i];
+            }
+        }
+
+        if(sum1==sum2)
+        {
+            return 0;
+        }
+        return sum1>sum2? 1 : 2; 
+        
+    }
+}
